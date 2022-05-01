d526a3f8ae549833a0df63bc892f98011c678142
Lubo Sladok <lubomir.sladok@bsp.sk>
2022-05-01 14:03:31 +0200

    add aikau document workflow in share  with rhino workflow service in platform

---
+++ alfresco-migrator-share/src/main/resources/alfresco/web-extension/site-webscripts/org/bikesterdam/dam/document-workflow.get.js
@@ -0,0 +1,2 @@
+<import resource="classpath:alfresco/web-extension/site-webscripts/org/bikesterdam/dam/task-section.lib.js">
+<import resource="classpath:alfresco/web-extension/site-webscripts/org/bikesterdam/dam/document-workflow.lib.js">
